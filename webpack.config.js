30762e2ac3df80a06ad91a24acb03e5c6a782a44
11Candice11 <storieboek@gmail.com>
2024-10-12 10:07:15 +0200

    webpack index.html

--- webpack.config.js
+++ webpack.config.js
@@ -2,20 +2,11 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin'); // Import HtmlWebpackPlugin
 
 module.exports = {
-  entry: './src/index.js', // Entry point of your application
+  entry: './src/App.js', // Entry point of your application
   output: {
     filename: 'bundle.js', // Output file name
     path: path.resolve(__dirname, 'dist'), // Output directory
     clean: true, // Cleans the output directory before each build (Webpack 5+)
-  },
-  devServer: {
-    static: {
-      directory: path.join(__dirname, 'dist'), // Correct way to specify the static files directory
-    },
-    historyApiFallback: true,  // This tells the server to serve index.html for all routes
-    compress: true,  // Enable gzip compression for everything served
-    port: 8080,      // Port for the dev server
-    open: true       // Automatically opens the browser when the server starts
   },
   module: {
     rules: [
@@ -44,8 +35,8 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './dist/index.html', // Path to your HTML template file
-      filename: 'index.html', // Output file name
+      template: './src/index.html', // Ensure this points to the src directory
+      filename: 'index.html',
     }),
   ],
   mode: 'development', // Set the mode to 'development' for better debugging
