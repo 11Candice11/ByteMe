e31aed7108fa182f268d7a0b8d2cc53232b0123c
11Candice11 <storieboek@gmail.com>
2024-10-12 09:40:22 +0200

    webpack and workflows

--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,44 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin'); // Import HtmlWebpackPlugin
 
 module.exports = {
-  mode: 'development',
-  entry: './src/index.js',
+  entry: './src/App.js', // Entry point of your application
   output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'public'),
-  },
-  devServer: {
-    static: {
-      directory: path.join(__dirname, 'public'), // Correct way to specify the static files directory
-    },
-    historyApiFallback: true,  // This tells the server to serve index.html for all routes
-    compress: true,  // Enable gzip compression for everything served
-    port: 8080,      // Port for the dev server
-    open: true       // Automatically opens the browser when the server starts
+    filename: 'bundle.js', // Output file name
+    path: path.resolve(__dirname, 'dist'), // Output directory
+    clean: true, // Cleans the output directory before each build (Webpack 5+)
   },
   module: {
     rules: [
       {
-        test: /\.js$/,
+        test: /\.js$/, // Rule for JavaScript files
         exclude: /node_modules/,
         use: {
           loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+      {
+        test: /\.css$/, // Rule for CSS files
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)$/i, // Rule for image files
+        type: 'asset/resource', // Webpack 5+ handles assets with "asset/resource"
+        generator: {
+          filename: 'images/[name][hash][ext]', // Output path for images
         },
       },
     ],
   },
-  resolve: {
-    extensions: [".js", ".jsx"]
-  }
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './dist/index.html', // Path to your HTML template file
+      filename: 'index.html', // Output file name
+    }),
+  ],
+  mode: 'development', // Set the mode to 'development' for better debugging
+  devtool: 'source-map', // Enable source maps for easier debugging
 };
